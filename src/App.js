206878819d3fef24bcde918a889f2243dc267372
Jonathan <jon_mully@hotmail.com>
2020-04-23 19:41:26 +0100

    Added Mr Benn

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Scooby Doo!
+          Mr Benn!
         </p>
         <a
           className="App-link"
