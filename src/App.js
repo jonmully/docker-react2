beeb2b43a297248a8108390c47c4865d75640bed
Jonathan <jon_mully@hotmail.com>
2020-04-24 14:23:28 +0100

    added travis file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Mr Benn!
+          Magic Roundabout!
         </p>
         <a
           className="App-link"
